bdae0df5f8e4492e379c0e300f72c644f7c9544f
root <root@PC5.(none)>
2014-06-10 12:42:18 +0200

    Implementación Partida (1.0)

--- src/DomainLayer/DomainModel/Partida.java
+++ src/DomainLayer/DomainModel/Partida.java
@@ -1,7 +1,71 @@
 package DomainLayer.DomainModel;
 
 /**
- * Created by jedi on 10/06/14.
+ * @version 1.0
  */
 public class Partida {
+
+    private int idPartida;
+    private boolean acabada;
+    private boolean guanyada;
+    private int errors;
+    private Paraula teParaula;
+    private Jugador jugador;
+    private IEstrategiaPuntuacio estrategiaPuntuacio;
+
+    public int getIdPartida() {
+        return idPartida;
+    }
+
+    public boolean isAcabada() {
+        return acabada;
+    }
+
+    public boolean isGuanyada() {
+        return guanyada;
+    }
+
+    public int getErrors() {
+        return errors;
+    }
+
+    public Paraula getTeParaula() {
+        return teParaula;
+    }
+
+    public Jugador getJugador() {
+        return jugador;
+    }
+
+    public IEstrategiaPuntuacio getEstrategiaPuntuacio() {
+        return estrategiaPuntuacio;
+    }
+
+    public void setIdPartida(int idPartida) {
+        this.idPartida = idPartida;
+    }
+
+    public void setAcabada(boolean acabada) {
+        this.acabada = acabada;
+    }
+
+    public void setGuanyada(boolean guanyada) {
+        this.guanyada = guanyada;
+    }
+
+    public void setErrors(int errors) {
+        this.errors = errors;
+    }
+
+    public void setTeParaula(Paraula teParaula) {
+        this.teParaula = teParaula;
+    }
+
+    public void setJugador(Jugador jugador) {
+        this.jugador = jugador;
+    }
+
+    public void setEstrategiaPuntuacio(IEstrategiaPuntuacio estrategiaPuntuacio) {
+        this.estrategiaPuntuacio = estrategiaPuntuacio;
+    }
 }
