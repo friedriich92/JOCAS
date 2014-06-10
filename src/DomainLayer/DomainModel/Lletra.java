c7a72f6e9408f34d7d3e5fd2dae05a4a81de1090
joan barroso <joanbarroso@233.xarxa-10-183-87.xsf-upc.upc.edu>
2014-06-10 13:19:51 +0200

    Añadida enumeracion Lletra

---
+++ src/DomainLayer/DomainModel/Lletra.java
@@ -0,0 +1,9 @@
+package DomainLayer.DomainModel;
+import java.util.ArrayList;
+
+/**
+ * Created by joanbarroso on 10/06/14.
+ */
+public enum Lletra {
+    A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z;
+}
