f146f18767c0af0dd0effade8a60646938bac6c4
joan barroso <joanbarroso@MacBook-Pro-de-joan.local>
2014-06-14 09:44:10 +0200

    Añadidas capas de vista y de datos vacias

---
+++ src/DataLayer/CtrlJugador.java
@@ -0,0 +1,12 @@
+package DataLayer;
+
+/**
+ * Created by joanbarroso on 14/06/14.
+ */
+public class CtrlJugador {
+
+
+    private String get (String entry) {
+        return entry; //sin empezar;
+    }
+}
