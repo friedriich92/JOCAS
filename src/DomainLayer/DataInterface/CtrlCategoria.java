23e16a18a63a00ee8cf87831dac40ada46bece92
pedromorenoduran <pedromorenoduran7@gmail.com>
2014-06-20 15:27:47 +0200

    Update CtrlCategoria.java

--- src/DomainLayer/DataInterface/CtrlCategoria.java
+++ src/DomainLayer/DataInterface/CtrlCategoria.java
@@ -1,32 +1,14 @@
-package DataLayer;
+package DomainLayer.DataInterface;
 
 import DomainLayer.DomainModel.Categoria;
 import DomainLayer.DomainModel.Paraula;
-import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Created by joanbarroso on 14/06/14.
+ * Created by jedi on 10/06/14.
  */
-public class CtrlCategoria implements DomainLayer.DataInterface.CtrlCategoria {
-    @Override
-    public String[] obteNomCategories() {
-        //codigo para que deje compilar
-        String[] lista = null;
-        return lista;
-    }
-    
-    @Override
-    public Categoria get(String s) {
-        //codigo para que deje compilar
-        return null;
-    }
-    
-    @Override
-    public Paraula getParaulaRandom() {
-        //codigo para que deje compilar
-        return null;
-    }
-    
-    
+public interface CtrlCategoria {
+    public String[] obteNomCategories();
+    public Categoria get(String s);
+    public Paraula getParaulaRandom();
 }
