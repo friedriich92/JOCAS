210b64d20024edcb0f1bcfb45521d3769a86dc17
pedromorenoduran <pedromorenoduran7@gmail.com>
2014-06-22 17:02:03 +0200

    Update CtrlParaula.java

--- src/DataLayer/CtrlParaula.java
+++ src/DataLayer/CtrlParaula.java
@@ -26,7 +26,7 @@ public class CtrlParaula implements DomainLayer.DataInterface.CtrlParaula {
         sessionFactory = configuration.buildSessionFactory(serviceRegistry);
         session = sessionFactory.openSession();
         
-        String query = "SELECT * FROM Paraula WHERE categoria = " + cat;
+        String query = "SELECT * FROM Paraula WHERE Categoria = " + cat;
         
         return session.createSQLQuery(query).addEntity(Paraula.class).list();
     }
