f7389b2aa49be906aafe53c391799d184b18a932
pedromorenoduran <pedromorenoduran7@gmail.com>
2014-06-22 16:57:21 +0200

    Update CtrlParaula.java

--- src/DomainLayer/DataInterface/CtrlParaula.java
+++ src/DomainLayer/DataInterface/CtrlParaula.java
@@ -1,7 +1,10 @@
 package DomainLayer.DataInterface;
 
+import DomainLayer.DomainModel.*;
+
 /**
  * Created by jedi on 10/06/14.
  */
 public interface CtrlParaula {
+    public Paraula[] getParaulesByCategoria(String cat);
 }
