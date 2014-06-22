6267c9441f4448307c3a8bacc5697e428866fbf4
pedromorenoduran <pedromorenoduran7@gmail.com>
2014-06-22 16:39:46 +0200

    Update CtrlPartida.java

--- src/DataLayer/CtrlPartida.java
+++ src/DataLayer/CtrlPartida.java
@@ -1,6 +1,12 @@
 package DataLayer;
 
-import DomainLayer.DomainModel.Partida;
+import DomainLayer.DomainModel.*;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.service.ServiceRegistryBuilder;
+
 
 /**
  * Created by joanbarroso on 14/06/14.
@@ -8,12 +14,28 @@ import DomainLayer.DomainModel.Partida;
 public class CtrlPartida implements DomainLayer.DataInterface.CtrlPartida {
     @Override
     public Partida getPartida(int idP) {
-        //codigo para que deje compilar
-        Partida p = new Partida();
-        return p;
+        
+       //creacion de la Session (?)
+        
+       SessionFactory sessionFactory;
+       Session session;
+       Configuration configuration = new Configuration();
+       configuration.configure();
+       ServiceRegistry serviceRegistry = new ServiceRegistryBuilder().applySettings(configuration.getProperties()).buildServiceRegistry();
+       sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+       session = sessionFactory.openSession();
+        
+        String query = "SELECT * FROM Partida WHERE idPartida = " + idP;
+        return Session.createSQLQuery(query).addEntity(Partida.class);
     }
     
+    @Override
     public int[] ferJugada(int idP, int pos, String lletra) {
         return new int[]{0,1,2,3,4};
+    }
+    
+    @Override
+    public int[] crearPartida(Jugador jug, Paraula par) {
+        return null;   
     }
 }
