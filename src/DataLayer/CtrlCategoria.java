bb516ea1ceb20792fc7fd251566e728ee0f73195
pedromorenoduran <pedromorenoduran7@gmail.com>
2014-06-22 16:39:15 +0200

    Update CtrlCategoria.java

--- src/DataLayer/CtrlCategoria.java
+++ src/DataLayer/CtrlCategoria.java
@@ -4,29 +4,73 @@ import DomainLayer.DomainModel.Categoria;
 import DomainLayer.DomainModel.Paraula;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.service.ServiceRegistryBuilder;
+
 
 /**
  * Created by joanbarroso on 14/06/14.
  */
 public class CtrlCategoria implements DomainLayer.DataInterface.CtrlCategoria {
     @Override
-    public String[] obteNomCategories() {
-        //codigo para que deje compilar
-        String[] lista = null;
+    public String[] obteNomCategories() {       
+        //creacion de la Session (?)
+        
+        SessionFactory sessionFactory;
+        Session session;
+        Configuration configuration = new Configuration();
+        configuration.configure();
+        ServiceRegistry serviceRegistry = new ServiceRegistryBuilder().applySettings(configuration.getProperties()).buildServiceRegistry();
+        sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+        session = sessionFactory.openSession();
+        
+        String query = "SELECT nom FROM Categoria";
+        Object[] res = Session.createSQLQuery(query);
+
+        String[] lista = new String[res.length];
+        for (int i = 0; i < res.length; ++i) 
+            lista[i] = res[i].toString();
+
         return lista;
     }
     
     @Override
     public Categoria get(String s) {
-        //codigo para que deje compilar
-        return null;
+        
+        //creacion de la Session (?)
+        
+        SessionFactory sessionFactory;
+        Session session;
+        Configuration configuration = new Configuration();
+        configuration.configure();
+        ServiceRegistry serviceRegistry = new ServiceRegistryBuilder().applySettings(configuration.getProperties()).buildServiceRegistry();
+        sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+        session = sessionFactory.openSession();
+        
+        String query = "SELECT * FROM Categoria WHERE nom = " + s;
+        return Session.createSQLQuery(query).addEntity(Categoria.class);
     }
     
     @Override
     public Paraula getParaulaRandom() {
-        //codigo para que deje compilar
-        return null;
+        
+        //creacion de la Session (?)
+        
+        SessionFactory sessionFactory;
+        Session session;
+        Configuration configuration = new Configuration();
+        configuration.configure();
+        ServiceRegistry serviceRegistry = new ServiceRegistryBuilder().applySettings(configuration.getProperties()).buildServiceRegistry();
+        sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+        session = sessionFactory.openSession();
+        
+        String query = "SELECT * FROM Paraula ORDER BY RAND() LIMIT 1";
+        return Session.createSQLQuery(query).addEntity(Paraula.class);
+
     }
-    
     
 }
