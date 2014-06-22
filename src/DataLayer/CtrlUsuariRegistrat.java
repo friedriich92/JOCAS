bb8e62aa15985ba3dcdd80e3bb8061b9fa784504
pedromorenoduran <pedromorenoduran7@gmail.com>
2014-06-22 16:39:58 +0200

    Update CtrlUsuariRegistrat.java

--- src/DataLayer/CtrlUsuariRegistrat.java
+++ src/DataLayer/CtrlUsuariRegistrat.java
@@ -1,6 +1,12 @@
 package DataLayer;
 
+import DomainLayer.DomainModel.Jugador;
 import DomainLayer.DomainModel.UsuariRegistrat;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.service.ServiceRegistryBuilder;
 
 /**
  * Created by joanbarroso on 14/06/14.
@@ -8,9 +14,21 @@ import DomainLayer.DomainModel.UsuariRegistrat;
 public class CtrlUsuariRegistrat implements DomainLayer.DataInterface.CtrlUsuariRegistrat {
     @Override
     public UsuariRegistrat obtenirUsuariRegistrat(String userN) {
-        //codigo para que deje compilar
-        UsuariRegistrat UR = new UsuariRegistrat();
-        return UR;
+        
+       //creacion de la Session (?)
+        
+       SessionFactory sessionFactory;
+       Session session;
+       Configuration configuration = new Configuration();
+       configuration.configure();
+       ServiceRegistry serviceRegistry = new ServiceRegistryBuilder().applySettings(configuration.getProperties()).buildServiceRegistry();
+       sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+       session = sessionFactory.openSession();
+       
+       
+       String query = "SELECT * FROM UsuariRegistrat WHERE username = " + userN;
+       return session.createSQLQuery(query).addEntity(UsuariRegistrat.class);
+
     }
 
 
