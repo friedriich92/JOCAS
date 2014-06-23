08cbb67fe58f68f1ee418c682aa8c0eb6d67c99c
Joaquin Campos Colmenarejo <joaquin.campos@a5s108pc42.fib.upc.es>
2014-06-23 08:22:15 +0200

    Parametres Hibernate

--- src/DomainLayer/DomainModel/Parametres.java
+++ src/DomainLayer/DomainModel/Parametres.java
@@ -3,11 +3,14 @@ package DomainLayer.DomainModel;
 /**
  *  @version 1.0
  **/
+@Entity
+@Table(name="PARAMETRES")
 public class Parametres {
 
     private static Parametres instance;
+    @Column(name="MAX_ERRORS")
     private int nombreMaximErrors;
-    private int idPartida;
+    //private int idPartida;
 
     public static Parametres getInstance() {
         if (instance == null) {
@@ -24,16 +27,22 @@ public class Parametres {
         this.nombreMaximErrors = nombreMaximErrors;
     }
     
+    /*
     public int getIdPartida() {
         return idPartida;
     }
+    */
     
+    /*
     public void setIdPartida(int idP) {
         idPartida = idP;
     }
+    */
     
+    /*
     public int NovaPartida() {
         idPartida++;
         return idPartida;
     }
+    */
 }
