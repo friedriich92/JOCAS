6e6f939919ff184eedf796cf77d4765ba906eee2
pedromorenoduran <pedromorenoduran7@gmail.com>
2014-06-23 11:21:52 +0200

    Update CtrlPartida.java

--- src/DataLayer/CtrlPartida.java
+++ src/DataLayer/CtrlPartida.java
@@ -1,6 +1,7 @@
 package DataLayer;
 
 import DomainLayer.DomainModel.*;
+import java.util.List;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
@@ -25,16 +26,35 @@ public class CtrlPartida implements DomainLayer.DataInterface.CtrlPartida {
        sessionFactory = configuration.buildSessionFactory(serviceRegistry);
        session = sessionFactory.openSession();
        String query = "SELECT * FROM Partida WHERE idPartida = " + idP;
-       Partida p = session.createSQLQuery(query).addEntity(Partida.class);
+       SQLQuery q = session.createSQLQuery(query);
+       List<Object[]> entities = q.list();
+       Partida p = new Partida();
+       for (Object[] entity : entities) {
+           p.setIdPartida(Integer.parseInt(entity[0].toString()));
+           boolean b;
+           if (Integer.parseInt(entity[1].toString()) == 1) b = true;
+           else b = false;
+           p.setAcabada(b);
+           if (Integer.parseInt(entity[2].toString()) == 1) b = true;
+           else b = false;
+           p.setGuanyada(b);
+           p.setErrors(Integer.parseInt(entity[3].toString()));
+           
+           CtrlJugador cJ = new CtrlJugador();
+           Jugador j = cJ.obteJugador(entity[4].toString());
+           p.setJugador(j);
+           
+           CtrlParaula cP = new CtrlParaula();
+           Paraula p2 = cP.getParaula(entity[5].toString());
+           
+           p.setTeParaula(p2);
+       }
        session.close();
        sessionFactory.close();
        return p;
     }
     
-    @Override
-    public int[] ferJugada(int idP, int pos, String lletra) {
-        return new int[]{0,1,2,3,4};
-    }
+
     
     /*
     @Override
